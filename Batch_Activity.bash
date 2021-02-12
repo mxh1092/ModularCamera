9d5419fb73f05d67508da7fb7c2b08578bc788ac
Matthew Herald <mherald1@Matthews-MacBook-Pro-2.local>
2021-02-12 18:34:42 +0000

    Batch scripts

---
+++ Batch_Activity.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sbatch Run_Camera_Params.bash 1
+sbatch Run_Camera_Params.bash 5
+sbatch Run_Camera_Params.bash 10
+sbatch Run_Camera_Params.bash 20
+sbatch Run_Camera_Params.bash 30
+sbatch Run_Camera_Params.bash 40
+sbatch Run_Camera_Params.bash 50
+sbatch Run_Camera_Params.bash 60
+sbatch Run_Camera_Params.bash 70
+sbatch Run_Camera_Params.bash 80
+sbatch Run_Camera_Params.bash 100
+sbatch Run_Camera_Params.bash 115
+sbatch Run_Camera_Params.bash 130
+sbatch Run_Camera_Params.bash 145
+sbatch Run_Camera_Params.bash 160
+sbatch Run_Camera_Params.bash 175
+sbatch Run_Camera_Params.bash 190
+sbatch Run_Camera_Params.bash 205
+sbatch Run_Camera_Params.bash 225
+sbatch Run_Camera_Params.bash 245
+sbatch Run_Camera_Params.bash 265
+sbatch Run_Camera_Params.bash 285
+sbatch Run_Camera_Params.bash 305
+
+
+
+
+
